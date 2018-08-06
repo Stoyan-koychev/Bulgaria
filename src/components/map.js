bcab2f53fee3ea83e26334c901eded75194d7444
Stoyan Koychev <stoqnkoi4ev@abv.bg>
2018-08-06 10:30:22 +0300

    add markers and set center

--- src/components/map.js
+++ src/components/map.js
@@ -4,9 +4,38 @@ import {Map, GoogleMapReact, InfoWindow, Marker, GoogleApiWrapper} from 'google-
 
 
 export class GoogleMaps extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      points: []
+    }
+  }
+
+  savePoints = () => {
+    this.props.locations.map( obj => {
+      let points = [];
+      points.push(obj.position);
+      this.setState({ points });
+    });
+  }
+
   render () {
+
+    let bounds = new this.props.google.maps.LatLngBounds();
+    for (let i = 0; i < this.state.points.length; i++) {
+      bounds.extend(this.state.points[i])
+    }
+
     return (
-      <Map google={this.props.google} zoom={12}>
+      <Map 
+        google={this.props.google} 
+        initialCenter={{
+          lat: 42.9184297,
+          lng: 23.723127
+        }} 
+        zoom={7}
+        bounds={bounds}
+      >
 
         {this.props.locations.map( obj => (
         <Marker 
